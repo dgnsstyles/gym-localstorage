65d032fa87dbb065e61c8367d51e85b5ad9ee0e2
David Goren <davidgoren@MacBook-Pro-de-David.local>
2019-12-12 18:17:18 -0300

    agregado todo

---
+++ ejemplo.js
@@ -0,0 +1,10 @@
+const formularioCarlos = document.querySelector('.felipe');
+const formularioCarlos = document.querySelector('#id');
+const formularioCarlos = document.querySelector('li');
+
+
+document.body.innerHTML += `<div> `
+
+
+
+
